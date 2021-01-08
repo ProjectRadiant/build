287e94eac5096f4ea97d94b9b1be9e3f21337edf
Maitreya29 <maitreyapatni30@gmail.com>
2021-01-08 15:05:29 +0530

    Nezukofy Installer

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -268,11 +268,11 @@ class EdifyGenerator(object):
                                   security_patch, device):
     self.Print("----------------------------------------------")
     if is_plus:
-      self.Print("        PixelExperience (Plus edition)")
-      self.Print("                by jhenrique09")
+      self.Print("              NezukoOS")
+      self.Print("     by @Maitreya29 & @AtomicXZ")
     else:
-      self.Print("              PixelExperience")
-      self.Print("              by jhenrique09")
+      self.Print("              NezukoOS")
+      self.Print("     by @Maitreya29 & @AtomicXZ")
     self.Print("----------------------------------------------")
     self.Print(" Android version: %s"%(android_version))
     self.Print(" Build id: %s"%(build_id))
