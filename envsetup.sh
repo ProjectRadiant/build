5bc65967e75d0fdca525db9c59d246e69805dd64
Maitreya29 <maitreyapatni30@gmail.com>
2021-04-28 00:57:09 +0530

    Change from aosp to nezuko

--- envsetup.sh
+++ envsetup.sh
@@ -50,7 +50,7 @@ EOF
     local T=$(gettop)
     local A=""
     local i
-    for i in `cat $T/build/envsetup.sh $T/vendor/aosp/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
+    for i in `cat $T/build/envsetup.sh $T/vendor/nezuko/build/envsetup.sh | sed -n "/^[[:blank:]]*function /s/function \([a-z_]*\).*/\1/p" | sort | uniq`; do
       A="$A $i"
     done
     echo $A
@@ -61,8 +61,8 @@ function build_build_var_cache()
 {
     local T=$(gettop)
     # Grep out the variable names from the script.
-    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/aosp/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
-    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/aosp/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_vars=(`cat $T/build/envsetup.sh $T/vendor/nezuko/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
+    cached_abs_vars=(`cat $T/build/envsetup.sh $T/vendor/nezuko/build/envsetup.sh | tr '()' '  ' | awk '{for(i=1;i<=NF;i++) if($i~/get_abs_build_var/) print $(i+1)}' | sort -u | tr '\n' ' '`)
     # Call the build system to dump the "<val>=<value>" pairs as a shell script.
     build_dicts_script=`\builtin cd $T; build/soong/soong_ui.bash --dumpvars-mode \
                         --vars="${cached_vars[*]}" \
@@ -144,8 +144,8 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
-    if (echo -n $1 | grep -q -e "^aosp_") ; then
-        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^aosp_//g')
+    if (echo -n $1 | grep -q -e "^nezuko_") ; then
+        CUSTOM_BUILD=$(echo -n $1 | sed -e 's/^nezuko_//g')
     else
         CUSTOM_BUILD=
     fi
@@ -475,7 +475,7 @@ function chooseproduct()
     if [ "x$TARGET_PRODUCT" != x ] ; then
         default_value=$TARGET_PRODUCT
     else
-        default_value=aosp_arm
+        default_value=nezuko_arm
     fi
 
     export TARGET_BUILD_APPS=
@@ -696,13 +696,13 @@ function lunch()
         # if we can't find a product, try to grab it off the PixelExperience GitHub
         T=$(gettop)
         cd $T > /dev/null
-        vendor/aosp/build/tools/roomservice.py $product
+        vendor/nezuko/build/tools/roomservice.py $product
         cd - > /dev/null
         check_product $product
     else
         T=$(gettop)
         cd $T > /dev/null
-        vendor/aosp/build/tools/roomservice.py $product true
+        vendor/nezuko/build/tools/roomservice.py $product true
         cd - > /dev/null
     fi
 
@@ -787,13 +787,13 @@ function tapas()
         return
     fi
 
-    local product=aosp_arm
+    local product=nezuko_arm
     case $arch in
-      x86)    product=aosp_x86;;
-      mips)   product=aosp_mips;;
-      arm64)  product=aosp_arm64;;
-      x86_64) product=aosp_x86_64;;
-      mips64)  product=aosp_mips64;;
+      x86)    product=nezuko_x86;;
+      mips)   product=nezuko_mips;;
+      arm64)  product=nezuko_arm64;;
+      x86_64) product=nezuko_x86_64;;
+      mips64)  product=nezuko_mips64;;
     esac
     if [ -z "$variant" ]; then
         variant=eng
@@ -1684,4 +1684,4 @@ fi
 
 export ANDROID_BUILD_TOP=$(gettop)
 
-. $ANDROID_BUILD_TOP/vendor/aosp/build/envsetup.sh
+. $ANDROID_BUILD_TOP/vendor/nezuko/build/envsetup.sh
