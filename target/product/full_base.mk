PalladiumOS-Temp/platform_build

Fix inheritance of PRODUCT_PROPERTY_OVERRIDES for Palladium audio files
Otherwise the ones set in Lineage vendor will be overridden by these.

Change-Id: I3f87dfd009d8ddfd48972c15770599742b12c4af

Author: Abhisek Devkota <ciwrl@cyanogenmod.com>
Remove extra product property overrides

This appears to clash with vendor/lineage/common mk's for overrides; causes
a broken default alarm (and OnTheHunt is so bleh).

Change-Id: I5ec990d3c5ad56b573c45f1a3b20169c989851a0

target/product/full_base.mk
@@ -42,10 +42,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(PALLADIUM_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US

target/product/mainline.mk
@@ -32,8 +32,12 @@ else
 $(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
+ifeq ($(PALLADIUM_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
 
 PRODUCT_PACKAGES += \

ProjectRadiant/build

target/product/full_base.mk
@@ -42,10 +42,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0
 
+ifeq ($(NEZUKO_BUILD),)
 # Additional settings used in all AOSP builds
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US

target/product/mainline.mk
@@ -32,8 +32,12 @@ else
 $(call inherit-product, frameworks/base/data/sounds/AudioPackage14.mk)
 endif
 
+ifeq ($(NEZUKO_BUILD),)
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Ring_Synth_04.ogg \
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=true \
 
 PRODUCT_PACKAGES += \